495c7eb28ccbfd9649fe339808c56f155c3a8dc5
Shobha8 <shobhaprakash2013@gmail.com>
2023-04-14 13:05:29 -0400

    changed to 22 feature df2

--- plutommp/src/test/java/org/iitworkforce/selenium/java/DateFunctioanlity2.java
+++ plutommp/src/test/java/org/iitworkforce/selenium/java/DateFunctioanlity2.java
@@ -11,7 +11,7 @@ public class DateFunctioanlity2 {
 		//String result = d2.getFutureDate(10);
 		
 
-		String result=d2.getFutureDate(15,"MM/dd/yyyy");
+		String result=d2.getFutureDate(22,"MM/dd/yyyy");
 
 
 		System.out.println(result);
